49d60a407c20e19fb633a3b4239bd5ca65f0aa4f
Inderjeet1176 <kaurinderjeet455@gmail.com>
2024-11-09 23:55:05 +0100

    [red] Add test for pushing an item onto the stack

--- test_mystack.py
+++ test_mystack.py
@@ -5,3 +5,10 @@ class TestMyStack(unittest.TestCase):
     def test_create_empty_stack(self):
         stack = MyStack()
         self.assertTrue(stack.is_empty())
+    def test_push_to_stack(self):
+        stack = MyStack()
+        stack.push(10)
+        self.assertFalse(stack.is_empty())
+        self.assertEqual(stack.peek(), 10)
+
+
