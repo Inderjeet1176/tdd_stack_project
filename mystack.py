5659baabc945657e2a42caa93b1b4052e7476943
Inderjeet1176 <kaurinderjeet455@gmail.com>
2024-11-09 23:55:45 +0100

    [green] Implement push and peek methods

--- mystack.py
+++ mystack.py
@@ -4,3 +4,16 @@ class MyStack:
 
     def is_empty(self):
         return len(self.items) == 0
+class MyStack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def peek(self):
+        return self.items[-1] if not self.is_empty() else None
+
